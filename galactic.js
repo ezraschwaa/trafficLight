66a547f79a7ee22f9ccab2fb472c2596e7c1fc70
Ezra Schwartz <ezraschwartz@reed.edu>
2022-01-06 23:16:52 +0000

    Add and Subtract Light buttons are functional

--- galactic.js
+++ galactic.js
@@ -1,26 +1,18 @@
+let trafficBoxElement = document.querySelector(".trafficBox");
 let circles = document.querySelectorAll(".circle")
-const numOfCircles = circles.length;
-const colors = ['red','yellow','green']
+let numOfCircles = circles.length;
 let last_num = -1;
 
+const MIN_CIRCLES = 3;
 const BOX_SHADOW_STYLE = '0 0 10px 5px ';
+const TRAFFIC_BOX_HEIGHT_DEFAULT = 250;
 
 //Initialize variables for buttons
 let light_button = document.getElementById('lightButton');
 let resetButton = document.getElementById('resetButton');
-
-//create an object of circles:
-class light {
-	constructor(circle, color){
-		this.circle = circle;
-		this.color = color;
-	}
-}
-
-let trafficBox = [];
-for(i = 0; i<numOfCircles;i++){
-	trafficBox.push(new light(circles[i], colors[i]))
-};
+let addButton = document.getElementById('addButton');
+let subButton = document.getElementById('subButton');
+let newLightsButton = document.getElementById('newLightsButton');
 
 //Adds style.  Expects circle object and color string.
 const addStyle = (circle, color = '') => {
@@ -37,13 +29,42 @@ const resetStyle = circle => {
 //Returns random number between 0 and max circles
 const getRandomNum = () => Math.floor(Math.random()*numOfCircles);
 
+//Returns random RGB Color
+const getRandomRGB = () => Math.floor(Math.random()*255);
+const getRandomColor = () => 'rgb('+getRandomRGB()+','+getRandomRGB()+','+getRandomRGB()+')';
+
+//create an object of circles
+class light {
+	constructor(circle, color){
+		this.circle = circle;
+		this.color = color;
+	}
+}
+
+let trafficBox = [];
+for(i = 0; i<numOfCircles;i++){
+	trafficBox.push(new light(circles[i], getRandomColor()));
+};
+
+//Reset all trafficBox colors
+const newColors = () => {
+	for (var key in trafficBox){
+		trafficBox[key].color = getRandomColor();
+	}
+
+	if(last_num!==-1){
+		resetStyle(trafficBox[last_num].circle);
+		addStyle(trafficBox[last_num].circle,trafficBox[last_num].color)
+	}
+}
+
 //Event handler function for light button click event
 const lightRandomCircle = () => {
 	let randomNum;
 
 	//Reset last circle lit if one exists
 	if(last_num !== -1){
-		resetStyle(trafficBox[last_num].circle)
+		resetStyle(trafficBox[last_num].circle);
 	}
 
 	//Generate random number and save it
@@ -56,6 +77,33 @@ const lightRandomCircle = () => {
 	addStyle(trafficBox[randomNum].circle, trafficBox[randomNum].color)
 }
 
+//Add and subtract button event handlers
+const addLight = () => {
+	const newCircle = document.createElement("div");
+	newCircle.classList.add("circle");
+	trafficBox.push(new light(newCircle, getRandomColor()));
+	numOfCircles++;
+	trafficBoxElement.appendChild(newCircle);
+	let newHeight = TRAFFIC_BOX_HEIGHT_DEFAULT + (68*(trafficBox.length-3));
+	trafficBoxElement.style.height = `${newHeight}px`;
+
+	if (trafficBox.length > 5){
+		document.body.style.height = document.getElementById("body").clientHeight + 400;
+	}
+};
+
+const subLight = () => {
+	if(trafficBox.length > MIN_CIRCLES){
+		trafficBox.pop();
+		trafficBoxElement.removeChild(trafficBoxElement.lastElementChild);
+		numOfCircles--;
+
+		let newHeight = TRAFFIC_BOX_HEIGHT_DEFAULT + (68*(trafficBox.length-3));
+		trafficBoxElement.style.height = `${newHeight}px`;
+
+	}
+};
+
 //Add event handler for reset button
 const resetLights = () => {
 	for(var key in trafficBox) {
@@ -67,3 +115,6 @@ const resetLights = () => {
 //Event listeners
 lightButton.onclick = lightRandomCircle;
 resetButton.onclick = resetLights;
+addButton.onclick = addLight;
+subButton.onclick = subLight;
+newLightsButton.onclick = newColors;
